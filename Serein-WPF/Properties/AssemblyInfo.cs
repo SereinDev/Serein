4dbfcceadaf69f64405fc970af14de77e75c4715
Zaitonn <22679330@qq.com>
2022-08-11 22:40:03 +0800

    update

--- Serein-WPF/Properties/AssemblyInfo.cs
+++ Serein-WPF/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Serein-WPF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Aliyun")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Serein-WPF")]
-[assembly: AssemblyCopyright("Copyright © Aliyun 2022")]
+[assembly: AssemblyCopyright("Copyright © Zaitonn 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
