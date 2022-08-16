a57737ecfa88a5f48866434f76ee6173910d61d0
Zaitonn <22679330@qq.com>
2022-08-17 00:20:46 +0800

    feat: wpfui尝试

---
+++ Serein2/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Wpf.Ui;
+
+namespace Serein2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
