cec705bfffb85e6ad8564ad0241e4fd15817f690
Zaitonn <22679330@qq.com>
2022-08-17 23:53:02 +0800

    feat: 新增更多功能

---
+++ Serein2/Windows/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Wpf.Ui;
+using System;
+
+namespace Serein2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UiWindow_StateChanged(object sender, EventArgs e)
+        {
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+        }
+    }
+}
