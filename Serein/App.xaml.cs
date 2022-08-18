8ce7874cc1e5f6b504cd2e9292fa89a01a529e6d
Zaitonn <22679330@qq.com>
2022-08-18 14:38:18 +0800

    code: 暂存更改

---
+++ Serein/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Serein
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+    }
+}
