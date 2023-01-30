b3851422691211afee65b262ba5a5d8ec6e617d2
Zaitonn <103164490+Zaitonn@users.noreply.github.com>
2023-01-30 23:27:26 +0800

    docs: 添加阅读量统计

---
+++ assets/visitor.js
@@ -0,0 +1,12 @@
+(function () {
+    var visitor = function (hook, vm) {
+        hook.afterEach(function (html) {
+            return `<div id="${window.location.hash}" class="leancloud_visitors">
+            阅读量&nbsp;·&nbsp;<span class="leancloud-visitors-count">?</span>
+        </div>` + html;
+        });
+    };
+
+    $docsify = $docsify || {};
+    $docsify.plugins = [].concat(visitor, $docsify.plugins || []);
+})();
